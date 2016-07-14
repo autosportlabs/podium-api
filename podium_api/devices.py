9ff21267a2d18f6d634ea1346e71da65f2443ba6
Kovak <kovac1066@gmail.com>
2016-07-14 14:43:53 -0700

    add list endpoint for devices

--- podium_api/devices.py
+++ podium_api/devices.py
@@ -3,6 +3,7 @@
 from podium_api.async import make_request_custom_success, get_json_header_token
 from podium_api.types.device import get_device_from_json
 from podium_api.types.redirect import get_redirect_from_json
+from podium_api.types.paged_response import get_paged_response_from_json
 
 
 def make_device_update(token, device_uri, name=None,
@@ -57,6 +58,74 @@ def make_device_update(token, device_uri, name=None,
         body=body, header=header,
         data={'updated_uri': device_uri}
         )
+
+def make_devices_get(token, endpoint,
+                     start=None, per_page=None,
+                     expand=True,
+                     quiet=None, success_callback=None,
+                     redirect_callback=None,
+                     failure_callback=None, progress_callback=None):
+    '''
+    Request that returns a PodiumPagedRequest of PodiumDevice. 
+
+    Args:
+        token (PodiumToken): The authentication token for this session.
+
+        endpoint (str): the endpoint to make the request to.
+
+    Kwargs:
+        expand (bool): Expand all objects in response output. Defaults to True
+
+        quiet (object): If not None HTML layout will not render endpoint
+        description. Defaults to None.
+
+        success_callback (function): Callback for a successful request,
+        will have the signature:
+            on_success(PodiumPagedResponse)
+        Defaults to None.
+
+        failure_callback (function): Callback for failures and errors. 
+        Will have the signature:
+            on_failure(failure_type (string), result (dict), data (dict))
+        Values for failure type are: 'error', 'failure'. Defaults to None.
+
+        redirect_callback (function): Callback for redirect, 
+        Will have the signature:
+            on_redirect(result (dict), data (dict))
+        Defaults to None.
+
+        progress_callback (function): Callback for progress updates,
+        will have the signature:
+            on_progress(current_size (int), total_size (int), data (dict))
+        Defaults to None.
+
+        start (int): Starting index for events list. 0 indexed.
+
+        per_page (int): Number per page of results, max of 100.
+
+    Return:
+        UrlRequest: The request being made.
+
+    '''
+    params = {}
+    if expand is not None:
+        params['expand'] = expand
+    if quiet is not None:
+        params['quiet'] = quiet
+    if start is not None:
+        params['start'] = start
+    if per_page is not None:
+        per_page = min(per_page, 100)
+        params['per_page'] = per_page
+
+    header = get_json_header_token(token)
+    return make_request_custom_success(endpoint, devices_success_handler,
+                                       method="GET",
+                                       success_callback=success_callback,
+                                       failure_callback=failure_callback,
+                                       progress_callback=progress_callback,
+                                       redirect_callback=redirect_callback,
+                                       params=params, header=header)
 
 
 def make_device_create(token, name,
@@ -238,6 +307,31 @@ def device_delete_handler(req, results, data):
     '''
     if data['success_callback'] is not None:
         data['success_callback'](data['deleted_uri'])
+
+
+def devices_success_handler(req, results, data):
+    '''
+    Creates and returns a PodiumPagedResponse with PodiumEvent as the payload
+    to the success_callback found in data if there is one.
+
+    Called automatically by **make_events_get**.
+
+    Args:
+        req (UrlRequest): Instace of the request that was made.
+
+        results (dict): Dict returned by the request.
+
+        data (dict): Wildcard dict for containing data that needs to be passed
+        to the various callbacks of a request. Will contain at least a 
+        'success_callback' key.
+
+    Return:
+        None, this function instead calls a callback.
+
+    '''
+    if data['success_callback'] is not None:
+        data['success_callback'](get_paged_response_from_json(results,
+                                                              "devices"))
 
 
 def device_success_handler(req, results, data):
