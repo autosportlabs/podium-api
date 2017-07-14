174cf350293517534b9b00a227b266c2f0f5cad2
Brent Picasso <brent@autosportlabs.com>
2017-07-14 15:35:26 -0700

    move podium_api to podium-api

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'API for Podium Motorsports Platform http://podium.live',
   author = 'Autosport Labs',
   author_email = 'sales@autosportlabs.com',
-  url = 'https://github.com/autosportlabs/podium_api', 
-  download_url = 'https://github.com/autosportlabs/podium_api/archive/0.0.5.tar.gz', 
+  url = 'https://github.com/autosportlabs/podium-api', 
+  download_url = 'https://github.com/autosportlabs/podium-api/archive/0.0.5.tar.gz', 
   keywords = ['motorsports', 'telemetry', 'live streaming'], 
   classifiers = [],
 )
