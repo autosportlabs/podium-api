a3222c838478e45122fba80fc8a1433ca5473725
Brent Picasso <brent@autosportlabs.com>
2017-07-13 22:46:00 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'podium_api',
-  packages = ['podium_api'], 
-  version = '0.0.4',
+  packages = ['podium_api','podium_api.types'], 
+  version = '0.0.5',
   description = 'API for Podium Motorsports Platform http://podium.live',
   author = 'Autosport Labs',
   author_email = 'sales@autosportlabs.com',
   url = 'https://github.com/autosportlabs/podium_api', 
-  download_url = 'https://github.com/autosportlabs/podium_api/archive/0.0.4.tar.gz', 
+  download_url = 'https://github.com/autosportlabs/podium_api/archive/0.0.5.tar.gz', 
   keywords = ['motorsports', 'telemetry', 'live streaming'], 
   classifiers = [],
 )
