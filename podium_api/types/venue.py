0464a31fe0c96044429c2879314f0f78faea10ae
Brent Picasso <brent@autosportlabs.com>
2018-02-28 21:32:05 -0800

    venues API WIP

---
+++ podium_api/types/venue.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class PodiumVenue(object):
+    """
+    Object that represents a Venue.
+
+    **Attributes:**
+        **venue_id** (int): Venue Id
+
+        **uri** (string): URI for the Venue.
+
+        **name** (string): The Venue's name.
+    """
+    def __init__(self, venue_id, uri, name):
+        self.venue_id = venue_id
+        self.uri = uri
+        self.name = name
+
+
+def get_venue_from_json(json):
+    """
+    Returns a PodiumVenue object from the json dict received from podium api.
+
+    Args:
+        json (dict): Dict of data from REST api
+
+    Return:
+        PodiumVenue: The PodiumVenue object for the data.
+    """
+    print('venue json {}'.format(json))
+    return PodiumVenue(json['id'],
+                      json['URI'],
+                      'name'
+                      )
