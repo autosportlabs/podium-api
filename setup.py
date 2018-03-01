b70ee477b5dd71d669096816fa8cd9026257fc21
Brent Picasso <brent@autosportlabs.com>
2018-03-01 11:57:05 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'podium_api',
   packages = ['podium_api','podium_api.types'], 
-  version = '0.0.10',
+  version = '0.0.11',
   description = 'API for Podium Motorsports Platform http://podium.live',
   author = 'Autosport Labs',
   author_email = 'sales@autosportlabs.com',
   url = 'https://github.com/autosportlabs/podium-api', 
-  download_url = 'https://github.com/autosportlabs/podium-api/archive/0.0.10.tar.gz', 
+  download_url = 'https://github.com/autosportlabs/podium-api/archive/0.0.11.tar.gz', 
   keywords = ['motorsports', 'telemetry', 'live streaming'], 
   classifiers = [],
 )
