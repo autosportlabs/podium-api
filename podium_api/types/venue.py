7ca9a485c36998e59ff9ea414684ad5a95561499
Brent Picasso <brent@autosportlabs.com>
2018-03-01 11:56:16 -0800

    update venues object model

--- podium_api/types/venue.py
+++ podium_api/types/venue.py
@@ -12,11 +12,30 @@ class PodiumVenue(object):
 
         **name** (string): The Venue's name.
     """
-    def __init__(self, venue_id, uri, name):
+    def __init__(self, venue_id, uri, updated, created,
+                 name,
+                 centerpoint,
+                 country_code,
+                 configuration,
+                 track_map_array,
+                 start_finish,
+                 finish,
+                 sector_points,
+                 length
+                 ):
         self.venue_id = venue_id
         self.uri = uri
+        self.updated = updated
+        self.created = created
         self.name = name
-
+        self.centerpoint = centerpoint
+        self.country_code = country_code
+        self.configuration = configuration
+        self.track_map_array = track_map_array
+        self.start_finish = start_finish
+        self.finish = finish
+        self.sector_points = sector_points
+        self.length = length
 
 def get_venue_from_json(json):
     """
@@ -30,5 +49,15 @@ def get_venue_from_json(json):
     """
     return PodiumVenue(json['id'],
                       json['URI'],
-                      'name'
+                      json['updated'],
+                      json['created'],
+                      json.get('name', None),
+                      json.get('centerpoint', None),
+                      json.get('country_code', None),
+                      json.get('configuration', None),
+                      json.get('track_map_array', None),
+                      json.get('start_finish', None),
+                      json.get('finish', None),
+                      json.get('sector_points', None),
+                      json.get('length', None)
                       )
